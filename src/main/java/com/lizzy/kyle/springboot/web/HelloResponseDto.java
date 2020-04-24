c49e92ee00d4f4ec0231dc0e699e32af78bd008e
iyeong-gyo <whdnseowkd@naver.com>
2020-04-24 22:40:25 +0900

    20200424
    REALCODE
     ADD
       - Application.java
       - HelloController.java
       - HelloResponseDto.java
    
    TESTCODE
     ADD
      - HelloControllerTest.java
      - HelloResponseDtoTest.java
    
    ========================================
    
    basic Setting!!

---
+++ src/main/java/com/lizzy/kyle/springboot/web/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.lizzy.kyle.springboot.web;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
