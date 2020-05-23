c3ed70b3e0858ec6405ef53ef3ec0fa62e48744e
iyeong-gyo <whdnseowkd@naver.com>
2020-05-23 18:33:13 +0900

    AWS 에서 빌드오류 수정

--- src/main/java/com/lizzy/kyle/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/lizzy/kyle/springboot/config/auth/CustomOAuth2UserService.java
@@ -13,7 +13,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import org.springframework.security.oauth2.client.registration.;
+
 
 import javax.servlet.http.HttpSession;
 import java.util.Collection;
