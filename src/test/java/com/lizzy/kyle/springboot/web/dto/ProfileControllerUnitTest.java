b0c83721d168fd96c557135cc1dde8a07f4e7f14
iyeong-gyo <whdnseowkd@naver.com>
2020-06-03 21:50:42 +0900

    무중단 배포 스크립트
    ProfileControllerTest,
    PorfileControllerUnitTest
    
    추가

---
+++ src/test/java/com/lizzy/kyle/springboot/web/dto/ProfileControllerUnitTest.java
@@ -0,0 +1,69 @@
+package com.lizzy.kyle.springboot.web.dto;
+import com.lizzy.kyle.springboot.web.profileController;
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다(){
+
+        //given
+
+        String expectedProfile = "real";
+        MockEnvironment env =  new MockEnvironment();
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("oauth");
+        env.addActiveProfile("real-db");
+
+        profileController controller = new profileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+
+    }
+
+
+    @Test
+    public void real_profile이_없으면_첫번째가_조회된다(){
+
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        profileController controller = new profileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+
+    }
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다(){
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        profileController controller = new profileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+
+    }
+
+
+
+}
